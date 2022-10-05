82f2dd106c4825de2aa0f96901d46aa445396513
elruizqu <eruiz40639@ufide.ac.cr>
2022-10-04 20:49:50 -0600

    Update PaisService.java

--- Tienda/src/main/java/com/tienda/service/PaisService.java
+++ Tienda/src/main/java/com/tienda/service/PaisService.java
@@ -8,6 +8,7 @@ import com.tienda.entity.Pais;
 import com.tienda.repository.PaisRepository;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  *
