91e96c0a7d1b363e89c5de67f70f8bd6291d10be
elruizqu <eruiz40639@ufide.ac.cr>
2022-10-04 20:49:09 -0600

    Semana 4

---
+++ Tienda/src/main/java/com/tienda/repository/PaisRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.repository;
+
+import com.tienda.entity.Pais;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author elena
+ */
+@Repository
+public interface PaisRepository extends CrudRepository<Pais,Long>{ //java generics y hace todas las consultas en la base de datos
+    
+}
---
+++ Tienda/src/main/java/com/tienda/repository/PersonaRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tienda.repository;
+
+import com.tienda.entity.Persona;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author elena
+ */
+@Repository
+public interface PersonaRepository extends CrudRepository<Persona,Long>{ //java generics y hace todas las consultas en la base de datos
+    
+}
