8bfb4a926a7c24a9043815768e9bce22e28339cc
elruizqu <eruiz40639@ufide.ac.cr>
2022-10-20 23:21:11 -0600

    iticket

---
+++ Iticket/src/main/java/com/ITiket/repository/ArtistaRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ITiket.repository;
+
+import com.ITiket.entity.Artista;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author elena
+ */
+@Repository
+public interface ArtistaRepository extends CrudRepository <Artista,Long>{//consultas en la base de datos
+    
+}
---
+++ Iticket/src/main/java/com/ITiket/repository/ConciertoRepository.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.ITiket.repository;
+
+import com.ITiket.entity.Concierto;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author elena
+ */
+@Repository
+public interface ConciertoRepository extends CrudRepository <Concierto,Long>{ //consultas en la base de datos
+    
+}
