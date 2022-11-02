6c40b5e539cd743235d8db77e14fcc418877129a
elruizqu <eruiz40639@ufide.ac.cr>
2022-11-01 20:20:03 -0600

    Semana 8

---
+++ Tienda/src/main/java/com/tienda/WebConfig.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/*Locale: Representa es el lenguaje, la region geografica, variantes del dialecto/idioma, de un usuario.
+SessionLocaleResolver: guardar el Locale seleccionado por un usuario como atributo en el request HTTP
+LocaleChangeInterceptor: Detectar cualquier cambio de parte del usuairo hacia lo que el Locale*/
+package com.tienda;
+
+import java.util.Locale;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+/**
+ *
+ * @author elena
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer{
+    
+    @Bean
+    public SessionLocaleResolver localeResolver(){
+        var slr = new SessionLocaleResolver();
+        slr.setDefaultLocale(new Locale("es"));
+        return slr;
+    }
+    
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor(){
+        var lci = new LocaleChangeInterceptor();
+        lci.setParamName("lang");
+        return lci;
+    }
+    
+    @Override
+    public void addInterceptors(InterceptorRegistry registro){
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+    
+}
